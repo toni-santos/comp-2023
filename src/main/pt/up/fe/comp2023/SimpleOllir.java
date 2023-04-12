5cd2d37021c3ba5dbb2788f8681c9ca24af7a45e
toni-santos <a.oliveirasantos122@gmail.com>
2023-04-12 17:37:17 +0100

    ollir tests setup

--- src/main/pt/up/fe/comp2023/SimpleOllir.java
+++ src/main/pt/up/fe/comp2023/SimpleOllir.java
@@ -4,14 +4,18 @@ import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
 
+import java.util.ArrayList;
+
 public class SimpleOllir implements JmmOptimization {
     @Override
     public OllirResult toOllir(JmmSemanticsResult jmmSemanticsResult) {
 
         OllirGenerator ollirGenerator = new OllirGenerator(jmmSemanticsResult.getSymbolTable());
 
+        ollirGenerator.visit(jmmSemanticsResult.getRootNode());
 
+        String code = ollirGenerator.getCode();
 
-        return null;
+        return new OllirResult(jmmSemanticsResult, code, new ArrayList<>());
     }
 }
